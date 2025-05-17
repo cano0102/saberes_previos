2fa4262c9761c8ef1d621dfa26c04120aacb6d5f
cano0102 <canoanderson418@gmail.com>
2025-05-16 23:34:19 -0500

    ejercicio #12

--- saberes.py
+++ saberes.py
@@ -39,7 +39,25 @@ def calificacion_final():
 calificacion_final()
 
 
+#   12. Un maestro desea saber qué porcentaje de hombres y qué porcentaje de mujeres hay en un grupo de alumnos.
+
+
+def  porcentajes():
+
+    total_estudiantes = int(input("DAME EL NUMERO TOTAL DE ESTUDIANTES: "))
     
+    hombres = int(input("DAME EL NUMERO DE ESTUDIANTES DE HOMBRE"))
+
+    mujeres = total_estudiantes - hombres
+
+
+
+    porcentaje_uno= (total_estudiantes/hombres) * 100
+    porcentaje_dos = (total_estudiantes/mujeres) * 100
+
+    print("EL PORCENTAJES DE HOMBRES ES:", porcentaje_uno , "EL PORCENTAJE DE MUJERES ES:",porcentaje_dos)
+
+
 
 
 
