fe1727385a60da915d318d7462c977b5225f0706
cano0102 <canoanderson418@gmail.com>
2025-05-16 23:40:20 -0500

    ejercicio #13

--- saberes.py
+++ saberes.py
@@ -57,8 +57,20 @@ def  porcentajes():
 
     print("EL PORCENTAJES DE HOMBRES ES:", porcentaje_uno , "EL PORCENTAJE DE MUJERES ES:",porcentaje_dos)
 
+porcentajes()
 
 
+# 13. Dada las horas trabajadas de una persona y el valor por hora. Calcular su salario e imprimirlo. 
+
+def salario():
+    
+    VALOR_DE_HORAS = 10000
+    horas_trabajadas = int(input("DAME LAS HORAS TRABAJADAS "))
+    total_de_salario = VALOR_DE_HORAS * horas_trabajadas
+
+    print("EL SALARIO TUYO ES DE " , total_de_salario )
+
+salario()
 
 
 
