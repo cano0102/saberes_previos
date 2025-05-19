7229f1bdd649685c70410aaa828fb2317fb4a216
cano0102 <canoanderson418@gmail.com>
2025-05-18 19:28:50 -0500

    ejercicio #16

--- saberes.py
+++ saberes.py
@@ -121,7 +121,7 @@ while True:
         prod["nombre"] = "pantalon"
         prod["precio"] = 800000 
         productos_comprados.append(prod)
-       
+        
         print("COMPRA EXITOSA✅")
 
     elif opcion == 4:
@@ -217,6 +217,16 @@ agregar_venta()
 
 # 16. Suponga que un conductor le pide a usted que le haga un algoritmo para calcular cuánto le corresponde en un día trabajado, teniendo en cuenta que tiene derecho a el 19% del total recaudado.
 
+
+
+def calcular_cuanto_le_corresponde_por_lo_recaudado():
+    
+
+    total_recaudado = float(input("DAME EL TOTAL RECAUDADO DEL DIA"))
+
+    lo_que_le_corresponde = (total_recaudado*19) / 100
+
+    print("LO QUE TE CORRESPONDE ES :",lo_que_le_corresponde)
 
 
 
